a611d914cdcc9c176c6cf4b4154d02625ff11843
정구아 <wjdrndk96@gmail.com>
2022-03-14 13:27:12 +0900

    feat: saveVote, rank api 추가, requestDto 수정

--- backend/src/main/java/com/ssafy/api/controller/vote/dto/response/FindVoteResponseDto.java
+++ backend/src/main/java/com/ssafy/api/controller/vote/dto/response/FindVoteResponseDto.java
@@ -3,6 +3,7 @@ package com.ssafy.api.controller.vote.dto.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ssafy.api.controller.candidate.dto.response.FindCandidateResponseDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +11,6 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class FindVoteResponseDto {
@@ -25,4 +25,15 @@ public class FindVoteResponseDto {
     @NotNull
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     LocalDateTime endDate;
+
+    @Builder
+    public FindVoteResponseDto(List<FindCandidateResponseDto> list, String name, String content, LocalDateTime startDate, LocalDateTime endTi
+    ){
+        this.candidates = list;
+        this.content = content;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endTi;
+    }
+
 }
--- backend/src/main/java/com/ssafy/api/service/vote/VoteService.java
+++ backend/src/main/java/com/ssafy/api/service/vote/VoteService.java
@@ -1,4 +1,67 @@
 package com.ssafy.api.service.vote;
 
+import com.ssafy.api.controller.candidate.dto.request.SaveCandidateRequestDto;
+import com.ssafy.api.controller.candidate.dto.response.FindCandidateResponseDto;
+import com.ssafy.api.controller.exception.CustomException;
+import com.ssafy.api.controller.exception.ErrorCode;
+import com.ssafy.api.controller.vote.dto.request.SaveVoteRequestDto;
+import com.ssafy.api.controller.vote.dto.response.FindVoteResponseDto;
+import com.ssafy.api.queryrepository.VoteQueryRepository;
+import com.ssafy.core.entity.candidate.Candidate;
+import com.ssafy.core.entity.user.User;
+import com.ssafy.core.entity.vote.Vote;
+import com.ssafy.core.repository.candidate.CandidateRepository;
+import com.ssafy.core.repository.user.UserRepository;
+import com.ssafy.core.repository.vote.VoteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
 public class VoteService {
+    private final VoteRepository voteRepository;
+    private final UserRepository userRepository;
+    private final VoteQueryRepository voteQueryRepository;
+    private final CandidateRepository candidateRepository;
+
+    public Long saveVote(SaveVoteRequestDto requestDto){
+
+        //save vote
+        Vote vote = Vote.builder()
+                .name(requestDto.getName())
+//                .host(admin)
+                .content(requestDto.getContent())
+                .startDate(requestDto.getStartDate())
+                .endDate(requestDto.getEndDate())
+                .build();
+        Vote savedVote = voteRepository.save(vote);
+
+        //save candidate
+        for (SaveCandidateRequestDto request:
+                requestDto.getCandidates()) {
+            Candidate candidate = request.toEntity();
+            candidate.assignVote(savedVote);
+            candidateRepository.save(candidate);
+        }
+
+        return savedVote.getId();
+    }
+
+    public FindVoteResponseDto getRanking(Long id){
+        Vote vote = voteRepository.findById(id)
+                .orElseThrow(()->new RuntimeException());
+        List<FindCandidateResponseDto> list = voteQueryRepository.findCandidatesSortByVoteTotal(id);
+        FindVoteResponseDto responseDto = new FindVoteResponseDto(list, vote.getName(), vote.getContent(), vote.getStartDate(), vote.getEndDate());
+        return responseDto;
+    }
+
+    public void voteTo(Long candidateId, int vote){
+        Candidate candidate = candidateRepository.findById(candidateId).get();
+        candidate.addVote(vote);
+    }
 }
