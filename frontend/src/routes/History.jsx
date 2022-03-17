eb8c2454070bd4f5702088ef6ae7b03f53a69545
bruno <bruno95@naver.com>
2022-03-17 11:26:33 +0900

    [FE]feat: history progressing

--- frontend/src/routes/History.jsx
+++ frontend/src/routes/History.jsx
@@ -15,49 +15,17 @@ function History() {
       <Nav />
       <div className={styles.history_title}>History</div>
       <div className={styles.contents}>
-        <section className={styles.sec}>
-          <div className={styles.history_container}>
-            <ul className={styles.history_itemlist}>
-              <li className={styles.short_img}>
-                <img
-                  src={chunhyang}
-                  alt="img1"
-                  className={styles.history_item}
-                />
-              </li>
-              <li className={styles.long_img}>
-                <img src={apart} alt="img2" className={styles.history_item} />
-              </li>
-              <li className={styles.short_img}>
-                <img src={ev9} alt="img3" className={styles.history_item} />
-              </li>
-              <li className={styles.long_img}>
-                <img
-                  src={kClassic}
-                  alt="img4"
-                  className={styles.history_item}
-                />
-              </li>
-              <li className={styles.short_img}>
-                <img
-                  src={logoContest}
-                  alt="img5"
-                  className={styles.history_item}
-                />
-              </li>
-              <li className={styles.long_img}>
-                <img
-                  src={missKorea}
-                  alt="img6"
-                  className={styles.history_item}
-                />
-              </li>
-              <li className={styles.short_img}>
-                <img src={mrKorea} alt="img7" className={styles.history_item} />
-              </li>
-            </ul>
+        <div className={styles.history_container}>
+          <div className={styles.history_itemlist}>
+            <img src={chunhyang} alt="img1" className={styles.history_item} />
+            <img src={apart} alt="img2" className={styles.history_item} />
+            <img src={ev9} alt="img3" className={styles.history_item} />
+            <img src={kClassic} alt="img4" className={styles.history_item} />
+            <img src={logoContest} alt="img5" className={styles.history_item} />
+            <img src={missKorea} alt="img6" className={styles.history_item} />
+            <img src={mrKorea} alt="img7" className={styles.history_item} />
           </div>
-        </section>
+        </div>
       </div>
       <Footer />
     </div>
