75b0d22dda47a466af6a8396b199e4eedbdfe9fb
정구아 <wjdrndk96@gmail.com>
2022-03-21 13:31:24 +0900

    refactor: module-core Testclass를 public으로 변경, @SpringBootApplication 달아줌

---
+++ module/polling/module-core/src/test/java/com/polling/PollingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.polling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+public class PollingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
