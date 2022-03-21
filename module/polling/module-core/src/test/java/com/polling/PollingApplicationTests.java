39a5ad71c664b87466df60b24ae369f085a5219b
정주헌 <cherish8513@naver.com>
2022-03-21 14:21:22 +0900

    fix: override entity field 적용
    API 모듈, 코어 테스트 모듈에 @EnableJpaAuditing 적용

--- module/polling/module-core/src/test/java/com/polling/PollingApplicationTests.java
+++ module/polling/module-core/src/test/java/com/polling/PollingApplicationTests.java
@@ -3,7 +3,9 @@ package com.polling;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PollingApplicationTests {
 
