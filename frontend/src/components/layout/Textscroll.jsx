377938f388b1bbb23c031ac934676cd3f19ee2a7
Code-hyunwoo <chichyun92@gmail.com>
2022-03-23 04:29:09 +0900

    fix: Image, Css

--- frontend/src/components/layout/Textscroll.jsx
+++ frontend/src/components/layout/Textscroll.jsx
@@ -7,11 +7,8 @@ import "./Textscroll.css"
 function Textscroll() {
 
     return (
-    <div class="animated-text">
-      <div class="line">This is</div>
-      <div class="line">a blockchain-based</div>
-      <div class="line">Polling</div>
-      <div class="line">service</div>
+    <div class="loading">
+        <div class="line">hello</div>
     </div>
     );
 }
