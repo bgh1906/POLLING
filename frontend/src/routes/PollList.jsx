4775b241823f9cdaec94f4650a16d29221faa074
bruno <bruno95@naver.com>
2022-03-22 19:32:21 +0900

    [FE]feat: pollList responsive grid progressing.. desktop & tablet version completed

--- frontend/src/routes/PollList.jsx
+++ frontend/src/routes/PollList.jsx
@@ -17,6 +17,7 @@ import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Button from "@mui/material/Button";
+import { useMediaQuery } from "react-responsive";
 
 export default function PollList() {
   const navigate = useNavigate();
@@ -75,41 +76,109 @@ export default function PollList() {
   shuffle(itemData);
   // useEffect(() => {
   // }, [itemData]);
+  const isDesktop = useMediaQuery({ query: "(min-width: 1224px)" });
+  const isTablet = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 992px)" });
+
   return (
     <div>
       <Newnav />
       <div className={styles.polllist}>
         <div className={styles.list_title}>Poll List</div>
-        <Box sx={{ width: 1500, height: 1080 }}>
-          <ImageList variant="masonry" cols={4} gap={28}>
-            {itemData.map((item) => (
-              <ImageListItem key={item.img} className={styles.opened_item}>
-                <img
-                  src={`${item.img}?w=248&fit=crop&auto=format`}
-                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                  alt={item.title}
-                  loading="lazy"
-                  className={styles.opened_img}
-                />
-                <div className={styles.opened_info}>
-                  <div>제 91회 춘향제</div>
-                  <div>2022.03.03 ~ 2022.05.10</div>
-                </div>
-                <div className={styles.opened_button}>
-                  <Button
-                    variant="contained"
-                    size="large"
-                    style={{ backgroundColor: "#77A3A9" }}
-                    onClick={() => {
-                      navigate("/poll/:pollnum");
-                    }}
-                  >
-                    투표하기
-                  </Button>
-                </div>
-              </ImageListItem>
-            ))}
-          </ImageList>
+        <Box sx={{ width: "80vw", height: 1080 }}>
+          {isDesktop && (
+            <ImageList variant="masonry" cols={4} gap={28}>
+              {itemData.map((item) => (
+                <ImageListItem key={item.img} className={styles.opened_item}>
+                  <img
+                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.title}
+                    loading="lazy"
+                    className={styles.opened_img}
+                  />
+                  <div className={styles.opened_info}>
+                    <div>제 91회 춘향제</div>
+                    <div>2022.03.03 ~ 2022.05.10</div>
+                  </div>
+                  <div className={styles.opened_button}>
+                    <Button
+                      variant="contained"
+                      size="large"
+                      style={{ backgroundColor: "#77A3A9" }}
+                      onClick={() => {
+                        navigate("/poll/:pollnum");
+                      }}
+                    >
+                      투표하기
+                    </Button>
+                  </div>
+                </ImageListItem>
+              ))}
+            </ImageList>
+          )}
+          {isTablet && (
+            <ImageList variant="masonry" cols={3} gap={28}>
+              {itemData.map((item) => (
+                <ImageListItem key={item.img} className={styles.opened_item}>
+                  <img
+                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.title}
+                    loading="lazy"
+                    className={styles.opened_img}
+                  />
+                  <div className={styles.opened_info}>
+                    <div>제 91회 춘향제</div>
+                    <div>2022.03.03 ~ 2022.05.10</div>
+                  </div>
+                  <div className={styles.opened_button}>
+                    <Button
+                      variant="contained"
+                      size="large"
+                      style={{ backgroundColor: "#77A3A9" }}
+                      onClick={() => {
+                        navigate("/poll/:pollnum");
+                      }}
+                    >
+                      투표하기
+                    </Button>
+                  </div>
+                </ImageListItem>
+              ))}
+            </ImageList>
+          )}
+          {isMobile && (
+            <ImageList variant="masonry" cols={2} gap={28}>
+              {itemData.map((item) => (
+                <ImageListItem key={item.img} className={styles.opened_item}>
+                  <img
+                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.title}
+                    loading="lazy"
+                    className={styles.opened_img}
+                  />
+                  <div className={styles.opened_info}>
+                    <div>제 91회 춘향제</div>
+                    <div>2022.03.03 ~ 2022.05.10</div>
+                  </div>
+                  <div className={styles.opened_button}>
+                    <Button
+                      variant="contained"
+                      size="large"
+                      style={{ backgroundColor: "#77A3A9" }}
+                      onClick={() => {
+                        navigate("/poll/:pollnum");
+                      }}
+                    >
+                      투표하기
+                    </Button>
+                  </div>
+                </ImageListItem>
+              ))}
+            </ImageList>
+          )}
         </Box>
       </div>
       <Footer />
