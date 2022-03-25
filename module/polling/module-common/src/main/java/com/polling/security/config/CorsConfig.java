a9b1ec1b5b4438d9f49520d831983f07ca0b47aa
정구아 <wjdrndk96@gmail.com>
2022-03-25 19:36:11 +0900

    fix: CorsConfig에 exposedHeader 추가, WebMvsConfig 삭제

--- module/polling/module-common/src/main/java/com/polling/security/config/CorsConfig.java
+++ module/polling/module-common/src/main/java/com/polling/security/config/CorsConfig.java
@@ -16,11 +16,14 @@ public class CorsConfig {
    public CorsFilter corsFilter() {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
-      config.setAllowCredentials(true);
-      config.setAllowedOriginPatterns(Collections.singletonList("*"));
+      config.addAllowedOrigin("*");
       config.addAllowedHeader("*");
-      config.setAllowCredentials(true);
       config.addAllowedMethod("*");
+      config.setAllowedOriginPatterns(Collections.singletonList("*"));
+      config.setAllowCredentials(true);
+      config.setAllowCredentials(true);
+      config.addExposedHeader("refreshToken");
+      config.addExposedHeader("accessToken");
       config.setMaxAge(3600L);
       source.registerCorsConfiguration("/api/**", config);
       return new CorsFilter(source);
