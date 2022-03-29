72ef2f10d5e91acff01a6481663090e702116328
정구아 <wjdrndk96@gmail.com>
2022-03-29 13:35:42 +0900

    fix: add validate member

--- module/polling/module-api/src/main/java/com/polling/auth/controller/AuthenticationRestController.java
+++ module/polling/module-api/src/main/java/com/polling/auth/controller/AuthenticationRestController.java
@@ -11,17 +11,13 @@ import com.polling.exception.CustomException;
 import com.polling.repository.member.MemberRepository;
 import com.polling.security.service.RedisService;
 import io.swagger.annotations.ApiOperation;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Controller to authenticate users.
@@ -33,34 +29,34 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class AuthenticationRestController {
 
-  private final JwtTokenProvider jwtTokenProvider;
-  private final RedisService redisService;
-  private final MemberRepository memberRepository;
-  private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RedisService redisService;
+    private final MemberRepository memberRepository;
+    private final AuthService authService;
 
-  @Trace
-  @PostMapping
-  @ApiOperation(value = "Native 로그인")
-  public ResponseEntity<Void> authorize(@RequestBody LoginDto loginDto,
-      HttpServletResponse response) {
-    Member member = memberRepository.findByEmail(loginDto.getEmail())
-        .orElseThrow(() -> new CustomException(CustomErrorResult.USER_NOT_FOUND));
-    if (!member.getPassword().equals(loginDto.getPassword())) {
-      throw new CustomException(CustomErrorResult.USER_NOT_FOUND);
+    @Trace
+    @PostMapping
+    @ApiOperation(value = "Native 로그인")
+    public ResponseEntity<Void> authorize(@RequestBody LoginDto loginDto,
+                                          HttpServletResponse response) {
+        Member member = memberRepository.findByEmail(loginDto.getEmail())
+                .orElseThrow(() -> new CustomException(CustomErrorResult.USER_NOT_FOUND));
+        if (!member.getPassword().equals(loginDto.getPassword())) {
+            throw new CustomException(CustomErrorResult.USER_NOT_FOUND);
+        }
+        setTokenHeaderAndRedis(member, response);
+        return ResponseEntity.status(200).build();
     }
-    setTokenHeaderAndRedis(member, response);
-    return ResponseEntity.status(200).build();
-  }
 
-  @Trace
-  @PostMapping("/social")
-  @ApiOperation(value = "OAuth 회원 가입/로그인")
-  public ResponseEntity<Void> authorizeOAuth(@RequestBody AuthDto requestDto,
-      HttpServletResponse response) {
-    Member member = authService.auth(requestDto);
-    setTokenHeaderAndRedis(member, response);
-    return ResponseEntity.status(200).build();
-  }
+    @Trace
+    @PostMapping("/social")
+    @ApiOperation(value = "OAuth 회원 가입")
+    public ResponseEntity<Void> authorizeOAuth(@RequestBody AuthDto requestDto,
+                                               HttpServletResponse response) {
+        Member member = authService.auth(requestDto);
+        setTokenHeaderAndRedis(member, response);
+        return ResponseEntity.status(200).build();
+    }
 
    @Trace
    @PostMapping("/validate")
@@ -77,22 +73,39 @@ public class AuthenticationRestController {
       return ResponseEntity.status(200).body(responseDto);
    }
 
-  @GetMapping("/logout")
-  public ResponseEntity<Void> logout(HttpServletRequest request) {
-    redisService.delValues(request.getHeader("refreshToken"));
-    return ResponseEntity.status(200).build();
-  }
+    @Trace
+    @PostMapping("/social/validate")
+    @ApiOperation(value = "기존 카카오가입회원이면 jwt+member:true, 신입이면 member:false 반환")
+    public ResponseEntity<ValidateMemberResponseDto> validate(@RequestBody ValidateMemberRequestDto requestDto, HttpServletResponse response) {
+        ValidateMemberResponseDto responseDto = new ValidateMemberResponseDto();
+        Member member = authService.validate(requestDto);
+        if (member != null) {
+            responseDto.setMember(true);
+            setTokenHeaderAndRedis(member, response);
+        } else {
+            responseDto.setMember(false);
+        }
+        return ResponseEntity.status(200).body(responseDto);
+    }
 
-  public void setTokenHeaderAndRedis(Member member, HttpServletResponse response) {
-    MemberDto memberDto = MemberAndDtoAdapter.entityToDto(member);
-    String accessToken = jwtTokenProvider.createAccessToken(memberDto.getId().toString(),
-        memberDto.getMemberRole());
-    String refreshToken = jwtTokenProvider.createRefreshToken(memberDto.getId().toString(),
-        memberDto.getMemberRole());
-    jwtTokenProvider.setHeaderAccessToken(response, accessToken);
-    jwtTokenProvider.setHeaderRefreshToken(response, refreshToken);
 
-    // Redis 인메모리에 리프레시 토큰 저장
-    redisService.setValues(refreshToken, memberDto.getId());
-  }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        redisService.delValues(request.getHeader("refreshToken"));
+        return ResponseEntity.status(200).build();
+    }
+
+    public void setTokenHeaderAndRedis(Member member, HttpServletResponse response) {
+        MemberDto memberDto = MemberAndDtoAdapter.entityToDto(member);
+        String accessToken = jwtTokenProvider.createAccessToken(memberDto.getId().toString(),
+                memberDto.getMemberRole());
+        String refreshToken = jwtTokenProvider.createRefreshToken(memberDto.getId().toString(),
+                memberDto.getMemberRole());
+        jwtTokenProvider.setHeaderAccessToken(response, accessToken);
+        jwtTokenProvider.setHeaderRefreshToken(response, refreshToken);
+
+        // Redis 인메모리에 리프레시 토큰 저장
+        redisService.setValues(refreshToken, memberDto.getId());
+    }
 }
