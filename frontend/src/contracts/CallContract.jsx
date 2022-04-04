6eaf36aaa456c38b799d78e30474847d919b6310
bruno <bruno95@naver.com>
2022-04-04 14:40:08 +0900

    [FE]feat: 컨트랙트 함수 export 완료

--- frontend/src/contracts/CallContract.jsx
+++ frontend/src/contracts/CallContract.jsx
@@ -6,12 +6,9 @@ const ENDPOINT = "http://localhost:7545";
 
 export const web3 = new Web3(new Web3.providers.HttpProvider(ENDPOINT));
 
+//컨트랙트 배포주소
+
 const abi = [
-  {
-    inputs: [],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
   {
     inputs: [
       {
@@ -33,6 +30,29 @@ const abi = [
         type: "uint256",
       },
     ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "candidateIndex",
+        type: "uint8",
+      },
+      {
+        internalType: "uint8",
+        name: "voteCount",
+        type: "uint8",
+      },
+    ],
+    name: "voteForCandidate",
+    outputs: [],
     stateMutability: "nonpayable",
     type: "function",
   },
@@ -59,24 +79,6 @@ const abi = [
     inputs: [
       {
         internalType: "uint8",
-        name: "candidateIndex",
-        type: "uint8",
-      },
-      {
-        internalType: "uint8",
-        name: "voteCount",
-        type: "uint8",
-      },
-    ],
-    name: "voteForCandidate",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint8",
         name: "",
         type: "uint8",
       },
@@ -94,46 +96,68 @@ const abi = [
   },
 ];
 
-//컨트랙트 배포주소
-const CONTRACT_ADDRESS = "0xe78A0F7E598Cc8b0Bb87894B0F60dD2a88d6a8Ab";
+const CONTRACT_ADDRESS = "0x4cFB3F70BF6a80397C2e634e5bDd85BC0bb189EE";
+// EC2에서 배포된 CA : 0xCfEB869F69431e42cdB54A4F4f105C19C080A601
 //트랜젝션 보내는 유저지갑주소
 const account = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1";
 
 //후보자 인덱스와 후보자명 확인
+// 프론트와 연동할 때는 안쓸 함수, 단순 체크용
 export const getCandInfo = () => {
   const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   testContract.methods
-    .candidateList(0)
+    .candidateList(1)
     .call({ from: account })
     .then(console.log);
 };
+// getCandInfo();
 
 //후보자 등록
 export const add = () => {
   const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   testContract.methods
-    .addCandidates(1)
+    // 파라미터는 등록할 후보자 수
+    .addCandidates(10)
+    // 네트워크에서 반환해주는 값이 0, 9
+    // 이걸 받아서 백엔드로 보내잖아요
+    // 배지환 이현우 서승원 0, 1, 2 > candidateIndex
     .send({ from: account })
     .then(console.log);
 };
+// add 에서 return값 0,9 받아오는 함수 지환님이  새로 작성해서 넘겨주실 예정
 
 //투표 후보자 인덱스에게 몇표 투표 진행
 export const vote = () => {
   const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   testContract.methods
-    .voteForCandidate(2, 10)
+    .voteForCandidate(0, 5)
+    // 여기서 2가 candidateIndex
+    // 투표하기전에 특정후보자 조회에서 Index 받아오기
     .send({ from: account })
     .then(console.log);
+  // 트랜잭션 id 리턴해줌 = transaction hash
 };
+// vote();
 
 //후보자 인덱스 입력시 득표수 반환
 export const votesReceived = () => {
   const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   testContract.methods
-    .votesReceived(2)
+    .votesReceived(0)
     .call({ from: account })
     .then(console.log);
 };
+// votesReceived();
+
+//후보자 인덱스 반환
+export const candidateIndexStart = () => {
+  const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+  testContract.methods
+    .candidateIndexStart()
+    .call({ from: account })
+    .then(console.log);
+};
+// candidateIndexStart();
 
 // // 유저에게 비밀번호를 입력받아 지갑주소을 생성하고 해당비밀번호는 관리자가 알수없다.
 // // 주의사항 명시 계좌 비밀번호는 사이트에서 관리하지않습니다 잊어버리는경우 알수없으니 보관시 주의 바랍니다.
@@ -174,49 +198,10 @@ export const lockAccount = () => {
 
 // send();
 // call();
+// getCandInfo();
+// add();
 // vote();
 // votesReceived();
 
 // unlock();
 // lock();
-
-// <!DOCTYPE html>
-// <html>
-// <head>
-//   <meta charset="utf-8">
-//   <title>Web3.js 계정 생성</title>
-// </head>
-
-// <script src="https://cdn.jsdelivr.net/gh/ethereum/web3.js@1.0.0-beta.8/dist/web3.min.js"></script>
-
-// <body>
-
-// * 비밀번호: <input id="passwd" type="text" style="width: 200px;" value=""><br><br>
-// <button onclick="createAccount()">생성</button><br><br>
-// * 진행상태: <span id="status"></span><br>
-// * 계정 주소: <span id="account_addr"></span>
-
-// <script>
-
-// 	// var Web3 = require("web3");
-// 	var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-
-// 	document.getElementById('status').innerHTML = "Ready";
-
-// 	function createAccount() {
-// 		var password = document.getElementById('passwd').value;
-// 		if (password == "") {
-// 			alert("비밀번호를 입력하세요!");
-// 				return;
-// 		}
-// 		document.getElementById('status').innerHTML = "Pending";
-// 		web3.eth.personal.newAccount(password, function(err,r){
-// 			document.getElementById('account_addr').innerHTML = r;
-// 			document.getElementById('status').innerHTML = "Complete";
-// 			console.log(r);
-// 		});
-// 	}
-
-// </script>
-// </body>
-// </html>
--- frontend/src/routes/Blocktest.jsx
+++ frontend/src/routes/Blocktest.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { web3 } from "../contracts/CallContract";
-import Web3 from "web3";
 
 export default function Blocktest() {
   const [password, setPassword] = useState("");
@@ -10,51 +9,20 @@ export default function Blocktest() {
   };
   const CreateAccount = async (e) => {
     e.preventDefault();
-    initWeb3();
-    console.log(web3);
+    // initWeb3();
+    // console.log(web3);
     // let accounts = web3.eth.getAccounts();
     let accounts = await web3.eth.personal.newAccount(password);
-    console.log(accounts);
+    console.log("accounts : ", accounts);
     // 지금까지 생성된 계정 리스트로 쭉 나옴
     // console.log(web3.eth.personal.getAccounts());
     setUserAccount(accounts);
+    console.log("userAccount : ", userAccount);
 
     // let balance = web3.eth.getBalance(accounts[0]);
     // console.log(balance);
   };
 
-  const initWeb3 = async () => {
-    if (window.ethereum) {
-      console.log("Recent mode");
-      window.web3 = new Web3(window.ethereum);
-      try {
-        // Request account access if needed
-        await window.ethereum.enable();
-        // Acccounts now exposed
-        // web3.eth.sendTransaction({
-        /* ... */
-        // };
-      } catch (error) {
-        // User denied account access...
-        console.log("User denied account access error:"`${error}`);
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      console.log("Legacy mode");
-      window.web3 = new Web3(web3.currentProvider);
-      // Acccounts always exposed
-      // web3.eth.sendTransaction({
-      //   /* ... */
-      // });
-    }
-    // Non-dapp browsers...
-    else {
-      console.log(
-        "Non-Ethereum browser detected. You should consider trying MetaMask!"
-      );
-    }
-  };
   return (
     <>
       <form action="">
