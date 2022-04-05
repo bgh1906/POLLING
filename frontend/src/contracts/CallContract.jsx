6ce1649a16005906a114376953f5f8ebc9b33391
bruno <bruno95@naver.com>
2022-04-05 15:21:31 +0900

    [FE]feat: token발급 코드 추가

--- frontend/src/contracts/CallContract.jsx
+++ frontend/src/contracts/CallContract.jsx
@@ -147,8 +147,8 @@ export const account = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1";
 
 //후보자 인덱스와 후보자명 확인
 // 프론트와 연동할 때는 안쓸 함수, 단순 체크용
+const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
 export const getCandInfoBlock = () => {
-  const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   testContract.methods
     .candidateList(1)
     .call({ from: account })
@@ -158,7 +158,7 @@ export const getCandInfoBlock = () => {
 
 //후보자 등록
 export const registerBlock = (num) => {
-  const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+  // const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   const regi = testContract.methods
     .addCandidates(num)
     .send({ from: account, gas: 1000000 });
@@ -170,7 +170,7 @@ export const registerBlock = (num) => {
 
 //투표 후보자 인덱스에게 몇표 투표 진행
 export const voteBlock = (idx) => {
-  const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+  // const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   const vote = testContract.methods
     .voteForCandidate(idx, 1)
     // 여기서 2가 candidateIndex
@@ -184,7 +184,7 @@ export const voteBlock = (idx) => {
 
 //후보자 인덱스 입력시 득표수 반환
 export const totalVotesBlock = (idx) => {
-  const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+  // const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   const totalVotes = testContract.methods
     .votesReceived(idx)
     .call({ from: account });
@@ -195,7 +195,7 @@ export const totalVotesBlock = (idx) => {
 
 //후보자 인덱스 반환
 export const getStartIndexBlock = () => {
-  const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
+  // const testContract = new web3.eth.Contract(abi, CONTRACT_ADDRESS);
   const Index = testContract.methods
     .candidateIndexStart()
     .call({ from: account });
@@ -242,12 +242,327 @@ export const sendEth = () => {
   web3.eth.sendTransaction(tx);
 };
 
-// send();
-// call();
-// getCandInfo();
-// add();
-// vote();
-// votesReceived();
+const tokenAbi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "name_",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "symbol_",
+        type: "string",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+    ],
+    name: "allowance",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "decimals",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "subtractedValue",
+        type: "uint256",
+      },
+    ],
+    name: "decreaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "addedValue",
+        type: "uint256",
+      },
+    ],
+    name: "increaseAllowance",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transfer",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
 
-// unlock();
-// lock();
+//POL토큰 전송 과정
+const tokenContract = new web3.eth.Contract(tokenAbi, CONTRACT_ADDRESS);
+
+//보낼주소에서 val만큼의 토큰을 전송가능하도록 승인해준다.
+const approveAccount = () => {
+  // fromAddress = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"; //보낼주소
+  const val = 100; //보내는것을 승인할 금액
+  //account는 관리자주소
+  tokenContract.methods
+    .approve("fromAddress", val)
+    .send({ from: account })
+    .then(console.log);
+};
+
+//보내는주소에서 받는주소로 val만큼의 POL토큰을 보내준다.
+const sendPOL = () => {
+  // fromAddress = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"; //보내는주소
+  // toAddress = "0x82769faAC683cF4AE8A5846B49e83414772686D3"; //받는주소
+  const val = 100; //보낼금액
+  //account는 관리자주소
+  tokenContract.methods
+    .transferFrom("fromAddress", "toAddress", val)
+    .send({ from: account })
+    .then(console.log);
+};
+
+// 사용자 주소의 잔액 확인 (잔액 바뀔때마다 리렌더링 필요)
+const checkPOL = () => {
+  // Address = "0x82769faAC683cF4AE8A5846B49e83414772686D3"; //POL 토큰 잔액 조회할 주소
+  //account는 관리자주소
+  tokenContract.methods
+    .balanceOf("Address")
+    .call({ from: account })
+    .then(console.log);
+};
