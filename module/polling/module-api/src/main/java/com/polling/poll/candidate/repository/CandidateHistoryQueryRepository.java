063007a628690566fdb0f79f3ded881b0fce72f9
정구아 <wjdrndk96@gmail.com>
2022-04-05 15:19:27 +0900

    fix: 멤버 기준 history 조회시 투표 데이터 추가
    - nickname 삭제
    - history createDate, thumbnail, poll title, candidate name 추가

--- module/polling/module-api/src/main/java/com/polling/poll/candidate/repository/CandidateHistoryQueryRepository.java
+++ module/polling/module-api/src/main/java/com/polling/poll/candidate/repository/CandidateHistoryQueryRepository.java
@@ -1,5 +1,6 @@
 package com.polling.poll.candidate.repository;
 
+import com.polling.poll.candidate.dto.response.FindCandidateHistoryByMemberResponseDto;
 import com.polling.poll.candidate.dto.response.FindCandidateHistoryResponseDto;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,8 +10,8 @@ public interface CandidateHistoryQueryRepository {
   List<FindCandidateHistoryResponseDto> findByCandidateId(Long candidateId, int offset,
       int limit);
 
-  List<FindCandidateHistoryResponseDto> findByCandidateByMemberId(Long memberId, int offset,
-      int limit);
+  List<FindCandidateHistoryByMemberResponseDto> findByCandidateByMemberId(Long memberId, int offset,
+                                                                                      int limit);
 
   List<FindCandidateHistoryResponseDto> findByCandidateByPollId(Long pollId, int offset,
       int limit);
