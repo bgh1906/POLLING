685695858307af45ce4abb6effb0399f22f77fe7
bruno <bruno95@naver.com>
2022-04-06 13:54:10 +0900

    [FE]fix: 가입할때 default권한 ADMIN으로 변경

--- frontend/src/routes/Join2.jsx
+++ frontend/src/routes/Join2.jsx
@@ -280,7 +280,7 @@ function Join2() {
             nickname: nickname,
             password: password,
             phoneNumber: phone,
-            role: "ROLE_USER",
+            role: "ROLE_ADMIN",
             wallet: wallet,
           }
         )
